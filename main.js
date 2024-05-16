17c8af2359b36dc14f0a6fa1ae43bea653db4d65
Marcos "Barbis" Serrao Gomez <114957083+BarbisMSG@users.noreply.github.com>
2024-05-16 11:49:02 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ let btnSuperavit = document.getElementById("btnSuperavit");
 let resultados = document.getElementById("resultados");
 let calculoResultado = document.getElementById("calculoResultado");
 let calculoCalorico = document.getElementById("calculoCalorico");
+let cardResultado = document.getElementById("cardResultado");
 
 //Btn para el calculo de deficit
 btnDeficit.addEventListener("click", () => {
@@ -28,27 +29,21 @@ btnDeficit.addEventListener("click", () => {
     }
 });
 
-//Btn para el calculo de superavit
-btnSuperavit.addEventListener("click", () => {
-    if (
-        nombre.value == "" ||
-        peso.value == "" ||
-        alturaCm.value == "" ||
-        edad.value == "" ||
-        sexo.value == ""
-    ) {
-        alert("por favor, completá todos los campos");
-    } else {
-        resultados.style.display = "flex";
-        main.style.display = "none";
-        calculoSuperavit();
+const tmbCalculo = () => {
+    if (sexo.value == "femenino") {
+        alert("HOLA");
     }
-});
+};
 
 //Const para déficit calórico
 const calculoDeficit = () => {
     if (sexo.value == "femenino") {
         calculoResultado.innerHTML =
+            // `<h3>Hola ${nombre.value}</h3>
+            // <p>Tu calculo calórico para Deficit dio:</p>
+            // <p>1700 Calorias</p>
+            // <p>Tu consumo de proteina diaraia debe ser de :</p>
+            // <p>128gr</p>`;
             "Hola " +
             nombre.value +
             "Tu consumo de calorías diarias debe ser de: " +
@@ -75,6 +70,23 @@ const calculoDeficit = () => {
             peso.value * 1.6;
     }
 };
+
+//Btn para el calculo de superavit
+btnSuperavit.addEventListener("click", () => {
+    if (
+        nombre.value == "" ||
+        peso.value == "" ||
+        alturaCm.value == "" ||
+        edad.value == "" ||
+        sexo.value == ""
+    ) {
+        alert("por favor, completá todos los campos");
+    } else {
+        resultados.style.display = "flex";
+        main.style.display = "none";
+        calculoSuperavit();
+    }
+});
 
 //Const para superávit calórico
 const calculoSuperavit = () => {
